28a4a2e2f9302771ccb11490eea417c88569fc0d
adityagaur200 <115742009+adityagaur200@users.noreply.github.com>
2024-09-04 23:58:35 +0530

    Added Ui

--- CloShop_FrontEnd/src/components/Admin/AddProduct.jsx
+++ CloShop_FrontEnd/src/components/Admin/AddProduct.jsx
@@ -1,13 +1,15 @@
 import { Button, Stack, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
+import UploadImage from './UploadImage'
 
 const AddProduct = () => {
     const [inputs,setInputs] = useState({
         Description : '',
-        price : "",
+        Price : "",
         Quantity:"",
-        size:"",
+        Size:"",
         ImageUrl:"",
+        Product_Name:"",
 
     })
   return (
@@ -15,11 +17,12 @@ const AddProduct = () => {
         <Typography align='center'>
           Welcome Admin To CloShop!!!!
         </Typography>
+        <TextField id="standard-basic" label="Name" variant="standard" type='text' value={inputs.value} onChange={(e)=>{setInputs(...inputs,Product_Name=e.target.value)}}/>
         <TextField id="standard-basic" label="Description" variant="standard" type={'text'} value={inputs.value} onChange={(e)=>{setInputs(...inputs,Description=e.target.value)}}/>
         <TextField id="standard-basic" label="Price" variant="standard" type={'number'} value={inputs.value} onChange={(e)=>{setInputs(...inputs,Price=e.target.value)}}/>
         <TextField id="standard-basic" label="Quantity" variant="standard" type={'number'} value={inputs.value} onChange={(e)=>{setInputs(...inputs,Quantity=e.target.value)}}/>
-        <TextField id="standard-basic" label="size" variant="standard" type={'text'} value={inputs.value} onChange={(e)=>{setInputs(...inputs,size=e.target.value)}}/>
-        <TextField id="standard-basic" label="ImageUrl" variant="standard" type='url' value={inputs.value} onChange={(e)=>{setInputs(...inputs,ImageUrl=e.target.value)}}/>
+        <TextField id="standard-basic" label="size" variant="standard" type={'text'} value={inputs.value} onChange={(e)=>{setInputs(...inputs,Size=e.target.value)}}/>
+        <UploadImage/>
         <Button variant='outlined'>Add</Button>
     </Stack>
       
