3c7f3fe171ac6757766b661064fed097b33e6097
adityagaur200 <115742009+adityagaur200@users.noreply.github.com>
2024-10-01 01:13:08 +0530

    Fix minnor errors and sign up form to backend

--- BackEnd/src/main/java/com/closhop/backend/Config/SecurityConfig.java
+++ BackEnd/src/main/java/com/closhop/backend/Config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,7 +25,7 @@ public class SecurityConfig
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/All","/Products/Add","/login","/register","/Products/all","/Products/product/","/Products/product/{id}","/Products/search","/Cart/add","/Cart/delete","/order/All_Orders","/order/Place_Order","api/payment/verifyPayment","api/payment/createOrder").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request.requestMatchers("user/All","/Products/Add","/user/login","/user/register","/Products/all","/Products/product/","/Products/product/{id}","/Products/search","/Cart/add","/Cart/delete","/order/All_Orders","/order/Place_Order","api/payment/verifyPayment","api/payment/createOrder").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
--- BackEnd/src/main/java/com/closhop/backend/Controller/UserController.java
+++ BackEnd/src/main/java/com/closhop/backend/Controller/UserController.java
@@ -2,31 +2,38 @@ package com.closhop.backend.Controller;
 import com.closhop.backend.Models.Users;
 import com.closhop.backend.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user")
 public class UserController
 {
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Users register (@RequestBody Users user)
-    {
-        return userService.registerUser(user);
+    public ResponseEntity<Users> register(@RequestBody Users user) {
+        Users registeredUser = userService.registerUser(user);
+
+        if (registeredUser != null) {
+            return ResponseEntity.ok(registeredUser);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
     @PostMapping("/login")
     public String login(@RequestBody Users user) {
 
         return userService.verify(user);
     }
-
     @GetMapping("/All")
     public java.util.List<Users> allUser()
     {
         return userService.getAll();
+
     }
 }
